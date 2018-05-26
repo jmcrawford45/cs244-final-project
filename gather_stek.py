c759cad4845a74be5521757cae4fa6e5b3f51f0d
Jared Crawford <jmcrawford45@gmail.com>
2018-05-26 13:19:33 -0700

    cron test

--- gather_stek.py
+++ gather_stek.py
@@ -2,4 +2,4 @@ import os
 import datetime
 
 date = datetime.datetime.now().strftime('%Y%m%d')
-os.system('python parser.py | ~/go/bin/zgrab --port 443 --timeout 72 --senders 100 --tls-verbose --ca-file=cacert.pem --tls --output-file={}-stek.json --tls-verbose --tls-session-ticket'.format(date))
+os.system('python parser.py | ~/go/bin/zgrab --port 443 --timeout 30 --senders 100 --tls-verbose --ca-file=cacert.pem --tls --output-file={}-stek.json --tls-verbose --tls-session-ticket'.format(date))
